334be94b1a4782831dfcbc3302d4e58a1cf39462
Michael Freling <80940180+michael-freling@users.noreply.github.com>
2024-06-21 19:48:24 -0700

    Filter out contents by keywords

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,22 +1,17 @@
-// Send tip to content script via messaging
-
-const storage = chrome.storage.sync
+const storage = chrome.storage.sync;
 
 storage.set({
-    "words": [
-        "Test"
-    ]
-})
+  words: ["Mushoku Tensei", "Classroom of the Elite"],
+});
 
 // sendResponse doesn't work within an async function
 // https://developer.chrome.com/docs/extensions/develop/concepts/messaging#simple
 // Use onMessageExternal from webpages like a main world
 // https://developer.chrome.com/docs/extensions/develop/concepts/messaging#external-webpage
-chrome.runtime.onMessageExternal.addListener((message, sender, sendResponse) => {
+chrome.runtime.onMessageExternal.addListener(
+  (message, sender, sendResponse) => {
     // TODO: verify a sender
-    console.log({
-        message,
-    })
     storage.get("words").then(sendResponse);
     return true;
-});
+  }
+);
--- src/content_scripts/youtube.ts
+++ src/content_scripts/youtube.ts
@@ -1,34 +1,81 @@
+const extensionId = "elifmnfcgkjlbclbglicfkbcdomkibdb";
+
+class SpoilerFilter {
+  forbiddenWords: string[];
+
+  constructor(forbiddenWords) {
+    this.forbiddenWords = forbiddenWords.map((word) => {
+      return word.toLowerCase();
+    });
+  }
+
+  readContents(): Array<HTMLElement> {
+    // There are multiple #content elements in a different level,
+    // So, instead of just getting #content, filter contents by #contents > #content
+    const pageContents = document.querySelectorAll("#contents");
+    const contents = [];
+    pageContents.forEach((pageContent) => {
+      pageContent.querySelectorAll("#content").forEach((content) => {
+        contents.push(content);
+      });
+    });
+    return contents;
+  }
+
+  filter(contents: Array<HTMLElement>): Array<HTMLElement> {
+    const result = [];
+    contents.forEach((content) => {
+      const videoTitleElement: HTMLElement =
+        content.querySelector("#video-title");
+      if (videoTitleElement == null) {
+        return;
+      }
+
+      this.forbiddenWords.forEach((forbiddenWord) => {
+        const title: string = videoTitleElement.innerText;
+        if (title.toLowerCase().includes(forbiddenWord)) {
+          result.push(content);
+          return;
+        }
+      });
+    });
+    return result;
+  }
+}
+
 // Listen for messages from the main world
 window.addEventListener("load", async (event) => {
   try {
     // Sends a message to the service worker and receives a tip in response
-    const extensionId = "abjlgogdppkdbbinpgfhakcfejamakie"
     const { words } = await chrome.runtime.sendMessage(extensionId, {
-      event: event
+      event: event,
     });
     console.log({
-      words
-    })
+      words,
+    });
+
+    const filter = new SpoilerFilter(words);
+
+    // Filter contents every 5 seconds. This is because
+    // 1. Some contents are not available when a page is loaded
+    // 2. When scrolling a page, new contents also need to be filtered
+    setInterval(() => {
+      const contents = filter.readContents();
+      if (contents.length == 0) {
+        // At first, YouTube contents are not loaded when a window was loaded
+        // Reload the contents when a content isn't available
+        return;
+      }
+
+      const filteredContents = filter.filter(contents);
+      filteredContents.forEach((content) => {
+        content.innerHTML = "Removed by a spoiler filter";
+      });
+      console.debug("Completed to filter contents");
+    }, 5000);
   } catch (error) {
     console.log("error", {
-      error
-    })
+      error,
+    });
   }
 });
-
-/*
-(async () => {
-  try {
-    // Sends a message to the service worker and receives a tip in response
-    const extensionId = "abjlgogdppkdbbinpgfhakcfejamakie"
-    const { words } = await chrome.runtime.sendMessage(extensionId, { greeting: 'tip' });
-    console.log({
-      words
-    })
-  } catch (error) {
-    console.log("error", {
-      error
-    })
-  }
-})();
-*/
