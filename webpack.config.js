5a64388a417f43eaeda476ae248141ec09727af7
Michael Freling <80940180+michael-freling@users.noreply.github.com>
2024-06-22 20:31:35 -0700

    Use webpack for static files, not only TypeScript codes

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
 const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   devtool: "inline-source-map",
 
   entry: {
-    "background/index": "./src/background/index",
-    "content_scripts/youtube": "./src/content_scripts/youtube",
+    "js/background/index": "./src/background/index",
+    "js/content_scripts/youtube": "./src/content_scripts/youtube",
   },
   output: {
     filename: "[name].js",
-    path: path.resolve(__dirname, "build/js")
+    path: path.resolve(__dirname, "build")
   },
   resolve: {
     extensions: [".ts", ".tsx"]
@@ -22,5 +23,22 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "./manifest.json" },
+        {
+          from: "./assets",
+          to: "assets"
+        },
+        {
+          from: "./data",
+          to: "data"
+        }
+      ],
+    }),
+    // webpack-ext-loader doesn't work because of the same error as next issue
+    // https://github.com/SimplifyJobs/webpack-ext-reloader/issues/28#issuecomment-1812405678
+  ]
 }
